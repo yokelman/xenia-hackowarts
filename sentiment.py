7e3b92af5f2d16c34b1e1bec8abf49302fc3f638
Vedant Jadhav <52625714+yokelman@users.noreply.github.com>
2025-02-15 13:52:25 +0530

    added toxicity classifier and batch processing of pipelines

--- sentiment.py
+++ sentiment.py
@@ -17,6 +17,7 @@ from dotenv import load_dotenv
 # sentiment_pipeline = pipeline("sentiment-analysis")
 
 sentiment_pipeline = pipeline("sentiment-analysis", model="cardiffnlp/twitter-roberta-base-sentiment")
+classifier = pipeline("text-classification", model="unitary/toxic-bert")
 
 load_dotenv()
 # YouTube API key
@@ -54,19 +55,21 @@ while len(comments) < num_of_comments:
     request = youtube.commentThreads().list(
         part='snippet',
         videoId=video_id,
+        order='relevance',
         maxResults=100,  # You can fetch up to 100 comments per request
         pageToken=nextPageToken
     )
     response = request.execute()
     for item in response['items']:
         comment = item['snippet']['topLevelComment']['snippet']
+        # print(comment)
         # Check if the comment is not from the video uploader
         if comment['authorChannelId']['value'] != uploader_channel_id:
             comments.append(comment['textDisplay'])
     nextPageToken = response.get('nextPageToken')
     if not nextPageToken:
         break
-
+print(len(comments))
 hyperlink_pattern = re.compile(r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
 
 relevant_comments = []
@@ -78,11 +81,18 @@ for comment_text in comments:
             relevant_comments.append(comment_text)
 comments=relevant_comments
 
+toxic_comments = []
+results = classifier(comments, truncation=True, padding=True, max_length=512, batch_size=4)
+for result in results:
+    if result['label'] == 'toxic' and result['score'] > 0.5:  # If the comment is toxic
+        toxic_comments.append(comment)
+
 # CALCULATING SENTIMENTS ALL AT ONCE FOR MODEL 1
 # sentiments = sentiment_pipeline(comments)
 # print(len(comments))
 # for sentiment in sentiments:
 #     avg_sentiment += sentiment['score'] * (1 if sentiment['label'] == 'POSITIVE' else -1)
+
 # CALCULATING SENTIMENT ONE BY ONE FOR MODEL 1
 # for comment in comments:
 #   # try:
@@ -91,18 +101,29 @@ comments=relevant_comments
 #   # except:
 #   #   print(comment)
 
-# FOR MODEL 2
-for comment in comments:
-    # Ensure the pipeline processes the text with truncation and padding, and set max_length to avoid long inputs
-    sentiment = sentiment_pipeline(comment, truncation=True, padding=True, max_length=512)  # max_length=512 ensures truncation at the right length
-    # Calculate sentiment score (1 for POSITIVE, -1 for NEGATIVE)
-    if sentiment[0]['label'] == 'LABEL_0':
+# # FOR MODEL 2 ONE BY ONE
+# for comment in comments:
+#     # Ensure the pipeline processes the text with truncation and padding, and set max_length to avoid long inputs
+#     sentiment = sentiment_pipeline(comment, truncation=True, padding=True, max_length=512)  # max_length=512 ensures truncation at the right length
+#     # Calculate sentiment score (1 for POSITIVE, -1 for NEGATIVE)
+#     if sentiment[0]['label'] == 'LABEL_0':
+#       mult=-1
+#     elif sentiment[0]['label'] == 'LABEL_2':
+#       mult=1
+#     else:
+#       mult=0
+#     avg_sentiment += sentiment[0]['score'] * mult
+
+# BATCH PROCESSING FOR MODEL 2
+sentiments = sentiment_pipeline(comments, truncation=True, padding=True, max_length=512, batch_size=4)
+for sentiment in sentiments:
+    if sentiment['label'] == 'LABEL_0':
       mult=-1
-    elif sentiment[0]['label'] == 'LABEL_2':
+    elif sentiment['label'] == 'LABEL_2':
       mult=1
     else:
       mult=0
-    avg_sentiment += sentiment[0]['score'] * mult
+    avg_sentiment += sentiment['score'] * mult
 avg_sentiment /= len(comments)
 
 print("Sentiment score:", avg_sentiment)
